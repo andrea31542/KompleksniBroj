6a1f5de68446d6f31eac5500b44bf7ba3cc2f2b4
AndreaV <AndreaV@ANDREAVW10-NB>
2021-10-21 18:16:25 +0200

    Add project files.

---
+++ KompleksniBroj/Klasa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBroj
+{
+   class KompleksniBrojKlasa
+    {
+
+        public double RealniDio;
+        public double ImaginarniDio; //CTR + D kopiranje linije 
+        public KompleksniBrojKlasa(double realni, double imaginarni)
+        {
+            RealniDio = realni;
+            ImaginarniDio = imaginarni;
+
+        }
+        
+        public override string ToString()
+        {
+            return $"({RealniDio}, {ImaginarniDio})";  // $ string is variable, inside {}
+        }
+
+    }
+}
---
+++ KompleksniBroj/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KompleksniBroj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //deklarirati varijablu 
+            KompleksniBrojKlasa kbk1 = new KompleksniBrojKlasa(2, 4); //poziva konstruktor klase 
+            // prvo rezervacija mjesta, alociranje memorije odnosno adrese i onda poziv konstruktora 
+
+            KompleksniBrojStruktura kbs1 = new KompleksniBrojStruktura(3, 5);
+
+            KompleksniBrojKlasa kbk2 = kbk1;
+            KompleksniBrojStruktura kbs2 = kbs1;
+
+            Console.WriteLine($"kbk1={kbk1}");
+            Console.WriteLine($"kbk2={kbk2}");
+            Console.WriteLine($"kbs1={kbs1}");
+            Console.WriteLine($"kbs2={kbs2}");
+
+            kbk2.RealniDio = 25;
+            kbs2.ImaginarniDio = 58;
+
+            Console.WriteLine($"kbk1={kbk1}"); 
+            Console.WriteLine($"kbk2={kbk2}"); 
+            Console.WriteLine($"kbs1={kbs1}"); 
+            Console.WriteLine($"kbs2={kbs2}");
+
+            Console.ReadKey(false); 
+
+        }
+    }
+}
